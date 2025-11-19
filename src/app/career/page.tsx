9d1393f6add01cd67fed9a77db7f121644a8b948
snotat <snotatonline@gmail.com>
2025-11-19 22:57:34 +0100

    Add services pages

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        
+    <div className={`w-full`}>
+<section>
+    <div className="">
+        <span>OUR VISION</span>
+    </div>
+    <div className="">To provide services that are tailored to the participants preferences to enable them to have a full life in the community.</div>
+</section>
+
     </div>
   )
 }
+
+export default page

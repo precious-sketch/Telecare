06d0042936351971adc85fa1dde10c57b4ca5064
snotat <snotatonline@gmail.com>
2025-12-12 18:58:54 +0100

    Add contact page header

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -196,7 +196,7 @@ const Footer = () => {
     SERVICES
 </div>
 </div>
-        <div className="my-6 mb-0 flex flex-col md:flex-row justify-between items-center text-xs text-gray-500 border-t border-gray-500/30 pt-6 space-x-4">
+        <div className="my-6 mb-0 flex flex-col md:flex-row justify-between items-center text-xs  border-t border-gray-500/30 pt-6 space-x-4">
           <div className="flex flex-wrap justify-center md:justify-start gap-2 mb-3">
             <Link href="/policy/privacy" className="">
               Privacy Policy
